0119e306977077dfc59a183d0990f2f0c0df12fe
Zach Wilson <zwilson@oak.edu>
2025-10-14 15:12:44 -0500

    V 0.0.2
    - Added models index
    - Added routes for post
    - Started addition of Post model

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -17,3 +17,43 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
+
+// Get /api/posts
+// get all blog posts
+// Controller: getAllPosts
+router.get('/', postController.getAllPosts);
+
+// Get /api/posts/:id
+// Get a single post by the ID number
+// URL Param: id (number) - post ID
+// Controller: getPostById
+router.get('/:id', postController.getPostById);
+
+// POST /api/posts
+// Create a new blog post
+// Req Body: { title, content, author}
+// Controller: createPost
+router.post('/', postController.createPost);
+
+// PUT /api/posts/:id
+// Update an entire post (replacing all fields)
+// URL Param: id (number) - Post Id
+// Req Body: { title, content, author, published}
+// Controller: updatePost
+router.put('/:id', postController.updatePost);
+
+// PATCH /api/posts/:id/publish
+// Toggles the published status of a post
+// URL Param: id (number) - Post Id
+// No Req Body
+// Controller: togglePublish
+router.patch('/:id/publish', postController.togglePublish);
+
+// DELETE /api/posts/:id
+// Delete a post
+// URL Param: id (number) - Post Id
+// Controller: deletePost
+router.delete('/:id', postController.deletePost);
+
+// Export router to be usable in other parts of application
+module.exports = router;
